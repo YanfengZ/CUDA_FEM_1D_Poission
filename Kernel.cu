49c32dcc9402c5f42bb88906e6158e4db3259122
Yuchien Lin <39214153+MiracleSNeko@users.noreply.github.com>
2018-09-10 00:02:41 +0900

    分离main与kernel

--- Kernel.cu
+++ Kernel.cu
@@ -2,10 +2,7 @@
 #include <stdlib.h>
 #include 'kernal.h'
 
-int main(int args,char** argv) {
-    double [N][N] matrixA;
-    double [N] vectorB;
-    MatrixAssemble<<<>>>    
-    equationAnswer = LTSolver(matrixA, vectorB);
-    return 0;
+__global__
+void Kernel() {
+    
 }
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include 'kernal.h'
+
+int main(int args,char** argv) {
+    double [N][N] matrixA;
+    double [N] vectorB;
+       
+    equationAnswer = LTSolver(matrixA, vectorB);
+    return 0;
+}
